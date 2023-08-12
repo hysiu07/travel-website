d40d5d30c03c191fa391449dca6782784cf994c7
Daniel <hysiu07@gmail.com>
2023-08-12 08:01:39 +0200

    added pictures and jsons info

---
+++ src/data/directionInfo.ts
@@ -0,0 +1,43 @@
+const directionInfo = [
+    {
+        country: 'Marocco',
+        info: "Morocco enchants with its proximity to the ocean, impressive mountain ranges, and deserts. It's an ideal solution for a vacation for those who love sun and exoticism! What should you know about Morocco and why choosing this place for a vacation will be a perfect shot?",
+        img: '../img/marocco-direction.jpg'
+    },
+    {
+        country: 'Zanzibar',
+        info: "Escape to Zanzibar, a tropical paradise where turquoise waters meet white sands. Dive into the Indian Ocean's wonders, wander through Stone Town's historic alleys, and indulge in Swahili flavors. Zanzibar's blend of culture and natural beauty is a dream come true for beach lovers and adventurers alike.",
+        img: '../img/zanzibar-direction.jpg'
+    },
+    {
+        country: 'Cyprus',
+        info: "Discover Cyprus, a Mediterranean gem that blends stunning beaches with rich history. Explore ancient ruins in Paphos, relax on the golden shores of Limassol, and savor delicious Cypriot cuisine. With a mix of modern comforts and old-world charm, Cyprus offers a captivating escape.",
+        img: '../img/cyprus-direction.jpg'
+    },
+    {
+        country: 'Greece',
+        info: "Embark on a journey to Greece, where ancient myths and stunning landscapes converge. Explore the Acropolis in Athens, relax on the idyllic beaches of Santorini, and savor Mediterranean cuisine. Greece's beauty and history make it a timeless destination.",
+        img: '../img/greece-direction.jpg'
+    },
+    {
+        country: 'Türkiye',
+        info: "Experience Turkey's enchanting blend of East and West. Explore the vibrant bazaars of Istanbul, marvel at the ancient ruins of Ephesus, and unwind on the sun-soaked beaches of Antalya. With a rich history and diverse landscapes, Turkey offers a captivating journey.",
+        img: '../img/turkiye-direction.jpg'
+    },
+    {
+        country: 'Thailand',
+        info: "Discover the allure of Thailand, a land of golden temples, lush jungles, and pristine beaches. Immerse yourself in the bustling markets of Bangkok, unwind on the serene islands of Phuket, and savor the vibrant street food. Thailand's rich culture and natural beauty promise an unforgettable journey.",
+        img: '../img/thailand-direction.jpg'
+    },
+    {
+        country: 'Dominican Republic',
+        info: "Escape to the Dominican Republic, a Caribbean paradise where white sands meet turquoise waters. Explore the historic streets of Santo Domingo, lounge on the palm-fringed beaches of Punta Cana, and savor the flavors of local cuisine. With its tropical charm and vibrant culture, the Dominican Republic offers a dreamy getaway.",
+        img: '../img/dominican-direction.jpg'
+    },
+    {
+        country: 'Spain',
+        info: "Experience the enchantment of Spain, a country where vibrant culture and diverse landscapes create a captivating tapestry. Wander through the historic streets of Barcelona, soak in the flamenco rhythms of Seville, and bask in the Mediterranean sun on the Costa del Sol. Spain's rich history, artistic heritage, and warm hospitality make it a timeless destination.",
+        img: '../img/spain-direction.jpg'
+    },
+    
+]
