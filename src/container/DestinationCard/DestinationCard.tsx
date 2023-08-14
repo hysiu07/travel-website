6d1260ab189458281442dd03b7adfef7abc5c5c9
Daniel <hysiu07@gmail.com>
2023-08-14 21:31:55 +0200

    problem with path img

--- src/container/DestinationCard/DestinationCard.tsx
+++ src/container/DestinationCard/DestinationCard.tsx
@@ -1,22 +1,33 @@
-import './DestinationCard.scss'
+import './DestinationCard.scss';
+
+import Img from '../../img/cyprus-direction.jpeg';
+
 type DestinationProps = {
 	text: string;
 	country: string;
 	img: string;
 };
 function DestinationCard({ text, country, img }: DestinationProps) {
-	console.log(img);
+	const image = img;
+	console.log(image);
 	return (
 		<div className='destinations__card'>
-			<div
+			<img
+				src={`${require("../../img/cyprus-direction.jpeg")}`}
 				className='card-img'
+
 				// style={{
-				// 	backgroundImage: `url('../../img/cyprus-direction.jpeg')`,
+				// 	backgroundImage: `url(${`'../../img/cyprus-direction.jpeg'`})`,
 				// }}
-			></div>
+			></img>
 			<h3 className='card-country'>{country}</h3>
 			<div className='card-text'>{text}</div>
 			<button className='card-button'>Check directions!</button>
+			{/* <div
+				className='ex'
+				style={{ backgroundImage: `${require('../../img/cyprus-direction.jpeg')}` }}
+            ></div> */}
+            <img src={img} alt=""  className='ex2'/>
 		</div>
 	);
 }
